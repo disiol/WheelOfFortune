f06a87326661dd077cdcfc8704d198ec181faafa
disiol <denisimus_games@ukr.net>
2023-02-17 00:18:06 +0200

    Mace show Surese ande safe

---
+++ Assets/Scripts/Game/Segment/GetCurrentSegmentName.cs
@@ -0,0 +1,16 @@
+using System;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Game.Segment
+{
+    public class GetCurrentSegmentName : MonoBehaviour
+    {
+        public string segmentName;
+
+        private void OnTriggerEnter2D(Collider2D col)
+        {
+            segmentName = col.name;
+        }
+    }
+}
